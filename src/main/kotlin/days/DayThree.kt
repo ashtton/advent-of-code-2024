8b0f08c5fd858e73888377f77be560dd46e79ebc
GleemingKnight <7jashton@gmail.com>
2024-12-03 10:18:51 -0500

    day 3

--- src/main/kotlin/days/DayThree.kt
+++ src/main/kotlin/days/DayThree.kt
@@ -3,24 +3,18 @@ package days
 import Challenge
 
 fun main() {
-    DayThree().execute()
+    DayThreePartTwo().execute()
 }
 
-class DayThree : Challenge(2024, 3, sample = true) {
-
+class DayThreePartOne : Challenge(2024, 3, sample = false) {
 
     val goodString = "mul(".split("")
     val nums = mutableListOf<Int>()
 
-    var numberOneString = ""
-    var numberTwoString = ""
-
     override fun readLine(str: String) {
         var builder = ""
-        numberOneString = ""
-        numberTwoString = ""
-
-
+        var numberOneString = ""
+        var numberTwoString = ""
 
         var comma = false
 
@@ -34,7 +28,6 @@ class DayThree : Challenge(2024, 3, sample = true) {
             }
 
             builder += s
-            println(builder)
 
             if (builder.length <= 4) {
                 if (s != goodString[builder.length]) {
@@ -49,29 +42,26 @@ class DayThree : Challenge(2024, 3, sample = true) {
             }
 
             if (s == ")") {
+                val numOne = numberOneString.toIntOrNull()
+                val numTwo = numberTwoString.toIntOrNull()
+
                 comma = false
                 numberOneString = ""
                 numberTwoString = ""
                 builder = ""
 
-                val numOne = numberOneString.toIntOrNull()
-                val numTwo = numberTwoString.toIntOrNull()
-
-                println("num: $numberOneString, numTwo: $numberTwoString, comma: $comma")
 
                 if (numOne == null || numTwo == null) {
                     return@forEach
                 }
 
-                nums.add(numberOneString.toInt() * numberTwoString.toInt())
+                nums.add(numOne * numTwo)
                 return@forEach
             }
 
 
             if (comma) {
                 numberOneString += s
-                println(numberOneString)
-
             } else {
                 numberTwoString += s
             }
@@ -82,11 +72,111 @@ class DayThree : Challenge(2024, 3, sample = true) {
                 numberOneString = ""
                 numberTwoString = ""
                 builder = s
+            }
+        }
+
+    }
+
+    override fun finish() {
+        println(nums.sum())
+    }
+}
+
+class DayThreePartTwo : Challenge(2024, 3, sample = false) {
+
+    val mulString = "mul(".split("")
+
+    val doString = "do()"
+    val dontString = "don't()"
+
+    val nums = mutableListOf<Int>()
+
+    var builder = ""
+    var numberOneString = ""
+    var numberTwoString = ""
+
+    var comma = false
+    var enabled = true
+
+    override fun readLine(str: String) {
+        str.split("").forEach { s ->
+            if (s.isEmpty()) {
                 return@forEach
             }
 
+            builder += s
+
+            if (builder.startsWith("d")) {
+                if (builder == doString) {
+                    enabled = true
+                    builder = ""
+                    return@forEach
+                }
+
+                if (builder == dontString) {
+                    enabled = false
+                    builder = ""
+                    return@forEach
+                }
+
+                if (!dontString.startsWith(builder) && !doString.startsWith(builder)) {
+                    builder = s
+                }
+
+                return@forEach
+            }
+
+            if (builder.startsWith("m")) {
+                if (builder.length <= 4) {
+                    if (s != mulString[builder.length]) {
+                        builder = s
+                    }
+                    return@forEach
+                }
+
+                if (s == ",") {
+                    comma = true
+                    return@forEach
+                }
+
+                if (s == ")") {
+                    val numOne = numberOneString.toIntOrNull()
+                    val numTwo = numberTwoString.toIntOrNull()
+
+                    comma = false
+                    numberOneString = ""
+                    numberTwoString = ""
+                    builder = ""
+
+                    if (numOne == null || numTwo == null) {
+                        return@forEach
+                    }
+
+                    if (enabled) {
+                        nums.add(numOne * numTwo)
+                    }
+                    return@forEach
+                }
 
 
+                if (comma) {
+                    numberOneString += s
+                } else {
+                    numberTwoString += s
+                }
+
+                if ((numberOneString.isNotEmpty() && numberOneString.toIntOrNull() == null) ||
+                    (numberTwoString.isNotEmpty() && numberTwoString.toIntOrNull() == null)) {
+                    comma = false
+                    numberOneString = ""
+                    numberTwoString = ""
+                    builder = s
+                }
+
+                return@forEach
+            }
+
+            builder = s
         }
 
     }
